7e85c5a255e11889210cfc07e7c42825baa4b8f5
Example <example@gmail.com>
2026-08-13 09:16:37 -0500

    Fue añadida y corregida la función que impedía que se mostrara la palanca activa, el error estaba en is_action_just_pressed

--- Scripts/palanca.gd
+++ Scripts/palanca.gd
@@ -3,11 +3,13 @@ extends Area2D
 var jugador_cerca = false
 
 func _on_body_entered(body):
-	jugador_cerca = true
+	if body.is_in_group("player"):
+		jugador_cerca = true
 
 func _on_body_exited(body):
-	jugador_cerca = false
+	if body.is_in_group("player"):
+		jugador_cerca = false
 
 func _process(delta):
-	if jugador_cerca and Input.is_action_just_pressed("action"):
+	if jugador_cerca and Input.is_action_pressed("action"):
 		print("Palanca activada")
