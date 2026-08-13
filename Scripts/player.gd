a542a2c3b847e21dd7f06d14dff4a1b1f407b161
Example <example@gmail.com>
2026-08-13 09:28:27 -0500

    Añadimos Sprites y animación de personaje Run

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
-
+@onready var anim = $AnimatedSprite2D
 @export var speed = 200.0
 @export var jump_force = -400.0
+
+func _ready():
+	add_to_group("player")
+	$AnimatedSprite2D.play("run")
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -12,4 +16,9 @@ func _physics_process(delta):
 
 	var dir = Input.get_axis("move_left", "move_right")
 	velocity.x = dir * speed
+	if dir != 0:
+		anim.flip_h = dir < 0
+		anim.play("walk")
+	else:
+		anim.play("idle")
 	move_and_slide()
